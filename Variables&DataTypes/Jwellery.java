fc5980ee33ab8fcc45d0de2ce41d67ab49742dd7
srujana15-12 <srujana1512@gmail.com>
2026-03-05 11:13:56 +0530

    variable and data type

---
+++ Variables&DataTypes/Jwellery.java
@@ -0,0 +1,15 @@
+class Jwellery
+{
+	public static void main(String[] args)
+    {
+		String choker="Pearls Silver Choker";
+		String metal="Silver";
+		String brand="Bevy Pearls";
+		String material="Silver,Pearl";
+	
+		System.out.println("Choker: "+choker);
+		System.out.println("Metal: "+metal);
+		System.out.println("Brand: "+brand);
+		System.out.println("Material: "+material);
+	}
+}
---
+++ Variables&DataTypes/Night.java
@@ -0,0 +1,30 @@
+class Night
+{
+	public static void main(String[] args)
+	{
+		String finishType =	"Unfinished" ;
+		String baseMaterial = "Silicone" ;
+		String bulbBase	= "E26 Medium" ;
+		String productDimensions = 	"6D x 5W x 5H Centimeters" ;
+		String itemWeight = "0.3 Kilograms" ;
+		String lampType =	"Night Light" ;
+		String shadeColour = "White" ;
+		String shadeMaterial =	"Silicone" ;
+		String switchType = "Touch" ;
+		String style =	"MODERN" ;
+		int price = 380 ;
+		
+		System.out.println("Finish Type:"+finishType);
+		System.out.println("BaseMaterial:"+baseMaterial);
+		System.out.println("Bulb Base:"+bulbBase);
+		System.out.println("Product Dimensions:"+productDimensions);
+		System.out.println("ItemWeight:"+itemWeight);
+		System.out.println("Lamp Type:"+lampType);
+		System.out.println("ShadeColour:"+shadeColour);
+		System.out.println("ShadeMaterial:"+shadeMaterial);
+		System.out.println("SwitchType"+switchType);
+		System.out.println("Style"+style);
+		System.out.println("ShadeMaterial:"+shadeMaterial);
+		System.out.println("Price is:"+price);
+	}
+}
